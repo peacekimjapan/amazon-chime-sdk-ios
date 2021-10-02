199f33cc18be71c23343f37ccf8202eeb60eabea
Peace <official@peacekim.com>
2021-10-02 18:07:52 +0900

    Added md5

--- Package.swift
+++ Package.swift
@@ -17,11 +17,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "AmazonChimeSDK",
-            url: "https://amazon-chime-sdk-ios.s3.amazonaws.com/sdk/0.16.5/AmazonChimeSDK-0.16.5.tar.gz"
+            url: "https://amazon-chime-sdk-ios.s3.amazonaws.com/sdk/0.16.5/AmazonChimeSDK-0.16.5.tar.gz",
+            checksum: "1527aff1111416dd5dc5d17659668618"
         ),
         .binaryTarget(
             name: "AmazonChimeSDKMedia",
-            path: "https://amazon-chime-sdk-ios.s3.amazonaws.com/media/0.12.1/AmazonChimeSDKMedia-0.12.1.tar.gz"
+            path: "https://amazon-chime-sdk-ios.s3.amazonaws.com/media/0.12.1/AmazonChimeSDKMedia-0.12.1.tar.gz",
+            checksum: "7b6bf631c95997010e54caa7068657b2"
         )
     ]
 )
