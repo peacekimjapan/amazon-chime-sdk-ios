6fc7827ba3afd16afae650bf350f68aef34cde38
Peace <official@peacekim.com>
2021-10-02 18:02:57 +0900

    Updated package swift

--- Package.swift
+++ Package.swift
@@ -10,10 +10,11 @@ let package = Package(
         .package(url: "https://github.com/birdrides/mockingbird.git", from: "0.16.0")
     ]
     targets: [
-        .target(name: "AmazonChimeSDK", path: "./AmazonChimeSDK/AmazonChimeSDK")
+        .target(name: "AmazonChimeSDK", path: "./AmazonChimeSDK/AmazonChimeSDK"),
         .testTarget(
             name: "AmazonChimeSDKTests",
-            dependencies: ["AmazonChimeSDK"]),
+            dependencies: ["AmazonChimeSDK"]
+        ),
         .binaryTarget(
             name: "AmazonChimeSDK",
             url: "https://amazon-chime-sdk-ios.s3.amazonaws.com/sdk/0.16.5/AmazonChimeSDK-0.16.5.tar.gz"
